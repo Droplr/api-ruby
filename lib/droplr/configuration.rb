b08c08b28cf61a48a4ec5ce73ae0790cceb8e6a2
Levi Nunnink <levi@droplr.com>
2014-11-10 08:55:51 -0800

    Add destruct fields to account edit fields

--- lib/droplr/configuration.rb
+++ lib/droplr/configuration.rb
@@ -23,7 +23,7 @@ module Droplr
     AUTH_ENDPOINT    = "/api/userinfo"
 
     # allowed values
-    EDIT_ACCOUNT_FIELDS = %w(password theme useDomain domain useRootRedirect rootRedirect dropPrivacy firstName lastName useLogo logo)
+    EDIT_ACCOUNT_FIELDS = %w(password theme useDomain domain useRootRedirect rootRedirect dropPrivacy firstName lastName useLogo logo, selfDestructType, selfDestructValue)
     LIST_DROPS_PARAMS   = %w(offset amount type sortBy order since until search)
     NOTE_VARIANTS       = %w(markdown textile code plain)
 
